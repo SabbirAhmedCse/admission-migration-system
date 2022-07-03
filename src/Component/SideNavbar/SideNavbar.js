c0cb31304f5a68ad00e5408892d8895d266571f4
Shongkor talukdar <shongkortalukdar2017@gmail.com>
2022-07-03 08:39:44 +0600

    inline style removed

--- src/Component/SideNavbar/SideNavbar.js
+++ src/Component/SideNavbar/SideNavbar.js
@@ -7,35 +7,24 @@ import { FcServices, FcAbout } from "react-icons/fc";
 import { MdOutlineDashboardCustomize } from "react-icons/md";
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 const SideNavbar = () => {
-    const blockquote = {
-        padding: "10px 55px",
-        backgroundColor: "#FAFAF9",
-        borderRadius: "25px",
-        color: "black",
-        boxShadow: "inset 0 -3em 3em rgba(0,0,0,0.1)",
-        "0 0  0 2px rgb(255,255,255)": "0.3em 0.3em 1em rgba(0,0,0,0.3)",
-      };
-      const Active = (e) => {
-        const url = e.target;
-        url.setAttribute("class","Blockquote");
-        console.log(url);
-      }
+
+
     return (
         <div>
             <div className="mt-2 text-start ps-4"> 
-            <NavLink to="/" style={{textDecoration: "none"}}><h6 onClick={(e)=>Active(e)} style={blockquote} >
+            <NavLink to="/" style={{textDecoration: "none"}}><h6  className="blockquote" >
               <AiOutlineHome className='me-2'></AiOutlineHome> Home
             </h6></NavLink>
-            <Link to="/applicant-list" style={{textDecoration: "none"}}><h6 onClick={(e)=>Active(e)} style={blockquote} >
+            <Link to="/applicant-list" style={{textDecoration: "none"}}><h6 className="blockquote" >
               <ImList2 className='me-2'> </ImList2>  ApplicantList
             </h6></Link>
-            <Link to="/dashboard" style={{textDecoration: "none"}}><h6 onClick={(e)=>Active(e)} style={blockquote} >
+            <Link to="/dashboard" style={{textDecoration: "none"}}><h6 className="blockquote" >
               <MdOutlineDashboardCustomize className='me-2'></MdOutlineDashboardCustomize> Dashboard
             </h6></Link>
-            <Link style={{textDecoration: "none"}}><h6 onClick={(e)=>Active(e)} style={blockquote} >
+            <Link style={{textDecoration: "none"}}><h6 className="blockquote" >
               <FcServices className='me-2'></FcServices> Product
             </h6></Link>
-            <Link style={{textDecoration: "none"}}><h6 onClick={(e)=>Active(e)} style={blockquote}>
+            <Link style={{textDecoration: "none"}}><h6 className="blockquote">
               <FcAbout className='me-2'></FcAbout> About
             </h6></Link>
             
