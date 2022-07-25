b678c79bc803f9a8812058509e477a66bb3cb725
Shongkor <shongkortalukdar2017@gmail.com>
2022-07-26 00:46:06 +0600

    all delete simple

--- src/Component/Delete/Delete.js
+++ src/Component/Delete/Delete.js
@@ -4,11 +4,25 @@ import DeleteFakeData from '../../FakeData/DeleteFakeData.json'
 
 const Delete = () => {
     const [data, setData] = useState(DeleteFakeData);
+    const [list, setList] = useState([]);
     const handleSingleDelete = (oneByOne) => {
         const newArray = data.filter(singleData => singleData !== oneByOne);
         setData(newArray)
-        console.log('oneByOne', oneByOne);
     }
+    const handleAllDelete = () => {
+        setData([])
+    };
+    const handleCheckBox = (oneByOne) => {
+        setList([...list, oneByOne])
+      
+    }
+    const deleteSelected =() => {
+        const update = [];
+        data.map(singleStudent => (
+             console.log(list.filter(singleData => singleData !== singleStudent))
+        ))
+        
+    };
     
     return(
         
@@ -33,7 +47,12 @@ const Delete = () => {
                                 {
                                     data.map(oneByOne => (
                                             <tr id={Math.random()}>
-                                                <td><input type='checkbox' class='checkbox_insideTBODY ' /></td>
+                                                <td td > < input type = 'checkbox'
+                                                class = 'checkbox_insideTBODY '
+                                                onChange = {
+                                                    () => handleCheckBox(oneByOne)
+                                                }
+                                                /></td >
                                                 <td>{oneByOne.Name} <span class='hiddenMessage_ifSkipped '>{oneByOne.Skip}</span></td>
                                                 <td>{oneByOne.Email}</td>
 
@@ -62,7 +81,14 @@ const Delete = () => {
                 <div class="card-body ">
 
                     <div class="form-group mr-5">
-                        <input type="submit" value='Delete All selected' class='btn btn-danger DeleteAllSelectedRow' />
+                        <input type="submit" value='Delete All' class='btn btn-danger DeleteAllSelectedRow' onClick={handleAllDelete}/>
+                        < input type = "submit"
+                        value = 'Delete All Selected'
+                        class = 'btn btn-danger DeleteAllSelectedRow'
+                        onClick = {
+                            deleteSelected
+                        }
+                        />
                     </div>
                 </div>
             </div>
